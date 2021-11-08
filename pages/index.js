944aed2bf2130cd1f2c25743091fe633904c0ed2
Thitiwat_T <te.thitiwat_st@tni.ac.th>
2021-11-08 16:41:22 +0700

    committ

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import ScheduleTable from '../components/Table'
-import EventModal from '../components/EventModal'
 import {
   Flex,
   Heading,
@@ -15,31 +14,33 @@ export default function Home() {
   const onChange = date => setDate(date)
 
   return (
-      <HStack spacing="24px">
-        <Flex
-          pos="sticky"
-          h="100vh"
-          boxShadow="0 0 24px 0 rgba(0, 0, 0, 0.15)"
-          w="300px"
-          flexDir="column"
-          bgColor="#C4C4C4"
-          justifyContent="space-between"
-        >
-          <Flex>
-            <Calendar
-              onChange={onChange}
-              value={date}
-            />
-          </Flex>
+    <HStack spacing="24px">
+      <Flex
+        pos="sticky"
+        h="100vh"
+        boxShadow="0 0 24px 0 rgba(0, 0, 0, 0.15)"
+        w="300px"
+        flexDir="column"
+        bgColor="#C4C4C4"
+        justifyContent="space-between"
+      >
+        <Flex>
+          <Calendar
+            onChange={onChange}
+            value={date}
+          />
         </Flex>
+      </Flex>
+      <Flex>
 
-        <VStack
-          spacing="24px"
-        >
-          <Heading fontSize="4xl">{date.toLocaleDateString("en-US")}</Heading>
-          <ScheduleTable />
-        </VStack>
-      </HStack>
+      </Flex>
+      <VStack
+        spacing="24px"
+      >
+        <Heading fontSize="4xl">{date.toLocaleDateString("en-US")}</Heading>
+        <ScheduleTable />
+      </VStack>
+    </HStack>
 
   )
 }
